29d8a16d9ae007197c257fc9107a13c7dd88f81d
Dell <Dell@DESKTOP-HPDJKCV>
2025-07-14 00:27:45 +0300

    Proje dosyası ekle.

---
+++ ButceTkip/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace ButceTkip
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(133, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(133, 108);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(133, 150);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(133, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 24);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "EKLE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(133, 232);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 24);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "SEÇİLENİ SİL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dataGridView1.Location = new System.Drawing.Point(600, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(701, 709);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "SEÇ";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "ADI";
+            this.Column2.HeaderText = "ADI";
+            this.Column2.Name = "Column2";
+            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "GELIR/GIDER";
+            this.Column3.HeaderText = "GELIR/GIDER";
+            this.Column3.Name = "Column3";
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "BILGI";
+            this.Column4.HeaderText = "BILGI";
+            this.Column4.Name = "Column4";
+            this.Column4.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column4.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "TARIH";
+            this.Column5.HeaderText = "TARIH";
+            this.Column5.Name = "Column5";
+            this.Column5.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column5.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "OCAK",
+            "ŞUBAT",
+            "MART",
+            "NİSAN",
+            "MAYIS",
+            "HAZİRAN",
+            "TEMMUZ",
+            "AĞUSTOS",
+            "EYLÜL",
+            "EKİM",
+            "KASIM",
+            "ARALIK"});
+            this.comboBox1.Location = new System.Drawing.Point(133, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Cyan;
+            this.label1.Location = new System.Drawing.Point(12, 687);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(90, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "ADI :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(16, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "GELİR / GİDER :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(77, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "BİLGİ :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(46, 339);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 15);
+            this.label5.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(217, 339);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 15);
+            this.label6.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(406, 339);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 15);
+            this.label7.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(133, 272);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 24);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "HESAPLA";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(463, 25);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Tümünü Seç";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1301, 709);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ ButceTkip/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Reflection.Emit;
+
+namespace ButceTkip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection baglan = new OleDbConnection("Provider = Microsoft.Ace.Oledb.12.0; Data Source = Aylar Tablosu.accdb");
+        void guncelle()
+        {
+            //RENKLENDİRME
+            foreach (DataGridViewRow satır in dataGridView1.Rows)
+            {
+                if (Convert.ToInt32(satır.Cells[2].Value) < 0)
+                {
+                    satır.DefaultCellStyle.BackColor = Color.Red;
+                }
+                else if (Convert.ToInt32(satır.Cells[2].Value) > 0)
+                {
+                    satır.DefaultCellStyle.BackColor = Color.LawnGreen;
+                }
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            guncelle();
+            label1.Text = "Kayıt Sayısı: " + dataGridView1.RowCount.ToString();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string[] ayTablolari = {
+        "Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
+        "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"};
+
+            if (comboBox1.SelectedIndex >= 0 && comboBox1.SelectedIndex < ayTablolari.Length)
+            {
+                string tabloAdi = ayTablolari[comboBox1.SelectedIndex];
+
+                try
+                {
+                    DataTable tablo = new DataTable();
+                    string sorgu = "SELECT * FROM [" + tabloAdi + "]";
+
+                    if (baglan.State == ConnectionState.Closed)
+                        baglan.Open();
+
+                    OleDbDataAdapter veriler = new OleDbDataAdapter(sorgu, baglan);
+                    veriler.Fill(tablo);
+                    dataGridView1.DataSource = tablo;
+                    guncelle();
+                    label1.Text = "Kayıt Sayısı: " + dataGridView1.RowCount.ToString();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hata oluştu: " + ex.Message);
+                }
+                finally
+                {
+                    if (baglan.State == ConnectionState.Open)
+                        baglan.Close();
+                }
+            }
+
+        }
+
+        public string query = "";
+        public string sql = "";
+        void kayıt()
+        {
+            OleDbCommand kaydet = new OleDbCommand(query, baglan);
+            kaydet.Parameters.AddWithValue("@A", textBox1.Text);
+            kaydet.Parameters.AddWithValue("@G", textBox2.Text);
+            kaydet.Parameters.AddWithValue("@B", textBox3.Text);
+            kaydet.Parameters.AddWithValue("@T", DateTime.Now);
+            baglan.Open();
+            kaydet.ExecuteNonQuery();
+            baglan.Close();
+        }
+
+
+        void getir()
+        {
+            DataTable tablo = new DataTable();
+            OleDbDataAdapter veriler = new OleDbDataAdapter(sql, baglan);
+            veriler.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            label1.Text = "Kayıt Sayısı: " + dataGridView1.RowCount.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == -1 || textBox1.Text == "" || textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("Geçerli ayı giriniz ve bilgileri eksiksiz giriniz");
+            }
+
+            else if (comboBox1.SelectedIndex == 0)
+            {
+                sql = "SELECT * FROM Ocak";
+                query = "INSERT INTO Ocak (ADI, [GELIR/GIDER], BILGI, TARIH) VALUES (@A,@G,@B,@T)";
+                kayıt(); getir(); guncelle();
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                sql = "SELECT * FROM Şubat";
+                query = "INSERT INTO Şubat (ADI, [GELIR/GIDER], BILGI, TARIH) VALUES (@A,@G,@B,@T)";
+                kayıt(); getir(); guncelle();
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                sql = "SELECT * FROM Mart";
+                query = "INSERT INTO Mart (ADI, [GELIR/GIDER], BILGI, TARIH) VALUES (@A,@G,@B,@T)";
+                kayıt(); getir(); guncelle();
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                sql = "SELECT * FROM Nisan";
+                query = "INSERT INTO Nisan (ADI, [GELIR/GIDER], BILGI, TARIH) VALUES (@A,@G,@B,@T)";
+                kayıt(); getir(); guncelle();
+            }
+            else if (comboBox1.SelectedIndex == 4)
+            {
+                sql = "SELECT * FROM Mayıs";
+                query = "INSERT INTO Mayıs (ADI, [GELIR/GIDER], BILGI, TARIH) VALUES (@A,@G,@B,@T)";
+                kayıt(); getir(); guncelle();
+            }
+            else if (comboBox1.SelectedIndex == 5)
+            {
+                sql = "SELECT * FROM Haziran";
+                query = "INSERT INTO Haziran (ADI, [GELIR/GIDER], BILGI, TARIH) VALUES (@A,@G,@B,@T)";
+                kayıt(); getir(); guncelle();
+            }
+            else if (comboBox1.SelectedIndex == 6)
+            {
+                sql = "SELECT * FROM Temmuz";
+                query = "INSERT INTO Temmuz (ADI, [GELIR/GIDER], BILGI, TARIH) VALUES (@A,@G,@B,@T)";
+                kayıt(); getir(); guncelle();
+            }
+            else if (comboBox1.SelectedIndex == 7)
+            {
+                sql = "SELECT * FROM Ağustos";
+                query = "INSERT INTO Ağustos (ADI, [GELIR/GIDER], BILGI, TARIH) VALUES (@A,@G,@B,@T)";
+                kayıt(); getir(); guncelle();
+            }
+            else if (comboBox1.SelectedIndex == 8)
+            {
+                sql = "SELECT * FROM Eylül";
+                query = "INSERT INTO Eylül (ADI, [GELIR/GIDER], BILGI, TARIH) VALUES (@A,@G,@B,@T)";
+                kayıt(); getir(); guncelle();
+            }
+            else if (comboBox1.SelectedIndex == 9)
+            {
+                sql = "SELECT * FROM Ekim";
+                query = "INSERT INTO Ekim (ADI, [GELIR/GIDER], BILGI, TARIH) VALUES (@A,@G,@B,@T)";
+                kayıt(); getir(); guncelle();
+            }
+            else if (comboBox1.SelectedIndex == 10)
+            {
+                sql = "SELECT * FROM Kasım";
+                query = "INSERT INTO Kasım (ADI, [GELIR/GIDER], BILGI, TARIH) VALUES (@A,@G,@B,@T)";
+                kayıt(); getir(); guncelle();
+            }
+            else if (comboBox1.SelectedIndex == 11)
+            {
+                sql = "SELECT * FROM Aralık";
+                query = "INSERT INTO Aralık (ADI, [GELIR/GIDER], BILGI, TARIH) VALUES (@A,@G,@B,@T)";
+                kayıt(); getir(); guncelle();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<DataGridViewRow> rowsToDelete = new List<DataGridViewRow>();
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                bool isChecked = false;
+                if (row.Cells["Column1"].Value != null)
+                    isChecked = Convert.ToBoolean(row.Cells["Column1"].Value);
+
+                if (isChecked)
+                {
+                    string ay = comboBox1.SelectedItem?.ToString() ?? "";
+                    string sorgu = "DELETE FROM [" + ay + "] WHERE ADI = @A AND [GELIR/GIDER] = @G";
+
+                    using (OleDbCommand cmd = new OleDbCommand(sorgu, baglan))
+                    {
+                        cmd.Parameters.AddWithValue("@A", row.Cells["Column2"].Value);
+                        cmd.Parameters.AddWithValue("@G", row.Cells["Column3"].Value);
+
+                        baglan.Open();
+                        cmd.ExecuteNonQuery();
+                        baglan.Close();
+                    }
+                    rowsToDelete.Add(row);
+                }
+            }
+            foreach (var row in rowsToDelete)
+            {
+                dataGridView1.Rows.Remove(row);
+            }
+            label1.Text = "Kayıt Sayısı: " + dataGridView1.RowCount.ToString();
+            guncelle(); 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            decimal gelir = 0;
+            decimal gider = 0;
+
+            foreach (DataGridViewRow sec in dataGridView1.Rows)
+            {
+                if (sec.IsNewRow) continue;
+
+                bool secili = false;
+                if (sec.Cells["Column1"].Value != null)
+                    Boolean.TryParse(sec.Cells["Column1"].Value.ToString(), out secili);
+
+                if (secili)
+                {
+                    if (sec.Cells[2].Value != null)
+                    {
+                        string degerStr = sec.Cells[2].Value.ToString();
+
+                        // Virgül ve noktalı sayı destekli kültürle parse et
+                        if (decimal.TryParse(degerStr,
+                            System.Globalization.NumberStyles.Number,
+                            System.Globalization.CultureInfo.CurrentCulture,
+                            out decimal deger))
+                        {
+                            if (deger >= 0)
+                                gelir += deger;
+                            else
+                                gider += Math.Abs(deger); // gideri pozitif topla
+                        }
+                    }
+                }
+
+                // Seçim sıfırlama
+                sec.Cells[0].Value = false;
+            }
+
+            decimal fark = gelir - gider;
+
+            // Label'lara yazdır
+            label5.Text = "Gelir = " + gelir.ToString("C2");
+            label6.Text = "Gider = " + gider.ToString("C2");
+            label7.Text = "Kalan = " + fark.ToString("C2");
+
+            dataGridView1.ClearSelection();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.Text == "Tümünü Seç")
+            {
+                foreach (DataGridViewRow sec in dataGridView1.Rows)
+                {
+                    sec.Cells[0].Value = true;
+                    button4.Text = "Tümünü Kaldır";
+                }
+            }
+            else if (button4.Text == "Tümünü Kaldır")     //else if yerine sadece if yazınca çalışmıyor çünkü biri bitince diğeri çalışıyor tekrar eski haline dönüyor kapiş
+            {
+                foreach (DataGridViewRow sec in dataGridView1.Rows)
+                {
+                    sec.Cells[0].Value = false;
+                    button4.Text = "Tümünü Seç";
+                }
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+    e.KeyChar != ',' && e.KeyChar != '-')
+            {
+                e.Handled = true;
+            }
+            if (e.KeyChar == '-' && ((sender as TextBox).SelectionStart != 0 || (sender as TextBox).Text.Contains("-")))
+            {
+                e.Handled = true;
+            }
+            if (e.KeyChar == ',' && (sender as TextBox).Text.Contains(","))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
+
